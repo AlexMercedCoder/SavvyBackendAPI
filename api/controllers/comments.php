fff8c47add5a3e68bfa2520418cdf3e79435a122
Alex Merced - AlexMercedCoder.com <alexmerced@alexmerced.com>
2019-12-13 18:00:55 -0500

    started on model and controller

---
+++ api/controllers/comments.php
@@ -0,0 +1,47 @@
+<?php
+
+
+header('Content-Type: application/json');
+
+include_once '../models/comments.php';
+
+if ($_REQUEST['action'] === 'index'){
+
+    echo json_encode(Comments::all());
+
+}
+
+
+else if ($_REQUEST['action'] === 'post'){
+
+    $request_body = file_get_contents('php://input');
+    var_dump($request_body);
+    $body_object = json_decode($request_body);
+    var_dump($body_object);
+    $new_comment = new Comment(null, $body_object->name,$body_object->estvalue,$body_object->events,$body_object->giver);
+    $all_comments = Comments::create($new_comment); //store the return value of People::create into a var
+
+    //send the return value of People::create (all people in the db) back to the user
+    echo json_encode($all_comments);
+}
+
+
+else if ($_REQUEST['action'] === 'update'){
+
+    $request_body = file_get_contents('php://input');
+    $body_object = json_decode($request_body);
+    $updated_comment = new Comment($_REQUEST['id'], $body_object->name, $body_object->estvalue,$body_object->events,$body_object->giver);
+    $all_comments = Comments::update($updated_comment);
+
+    echo json_encode($all_comments);
+}
+
+
+else if ($_REQUEST['action'] === 'delete'){
+
+    $all_comments = Comments::delete($_REQUEST['id']);
+    echo json_encode($all_comments);
+}
+
+
+?>
---
+++ api/models/comments.php
@@ -0,0 +1,90 @@
+<?PHP
+
+
+$dbconn = pg_connect("host=localhost dbrestid=recomment user=postgres password=database");
+// (id SERIAL, restid int, author varchar(30), comment varchar(140), password varchar(8), date timestamp)
+
+class Comment {
+    public $id;
+    public $restid;
+    public $author;
+    public $comment;
+    public $password;
+    public $date
+
+    public function __construct($id, $restid, $estValue, $comment, $password, $date) {
+        $this->id = $id;
+        $this->restid = $restid;
+        $this->author = $estValue;
+        $this->comment = $comment;
+        $this->password = $password;
+        $this->date = $date;
+    }
+}
+
+class Comments {
+
+
+
+    static function all(){
+        //create an empty array
+        $comments = array();
+
+        //query the database
+        $results = pg_query("SELECT * FROM comments");
+
+        $row_object = pg_fetch_object($results);
+        // var_dump($row_object);
+        while($row_object){
+
+                    $new_comment = new Comment(
+                        intval($row_object->id),
+                        intval($row_object->restid),
+                        intval$row_object->author,
+                        $row_object->comment,
+                        $row_object->password,
+                        $row_object->date
+
+                    );
+                    $comments[] = $new_comment;
+
+                    // print_r($comments);
+                    $row_object = pg_fetch_object($results);
+                }
+
+        // die(); //halt execution
+
+        // print_r($comments);
+        return $comments;
+    }
+
+
+    static function create($comment){
+        $query = "INSERT INTO comments (restid, author, comment, password) VALUES ($1, $2, $3, $4)";
+        $query_params = array($comment->restid, $comment->author, $comment->comment, $comment->password);
+        pg_query_params($query, $query_params);
+        return self::all();
+    }
+
+    static function update($updated_comment){
+        $query = "UPDATE comments SET restid = $1, author = $2, comment = $3, password=$4 WHERE id = $5";
+        $query_params = array($updated_comment->restid, $updated_comment->author, $updated_comment->comment, $updated_comment->password,$updated_comment->id);
+        $result = pg_query_params($query, $query_params);
+
+        return self::all();
+    }
+
+    static function delete($id){
+    $query = "DELETE FROM comments WHERE id = $1";
+    $query_params = array($id);
+    $result = pg_query_params($query, $query_params);
+
+    return self::all();
+    }
+
+
+
+}
+
+
+?>
