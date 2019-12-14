890457b85166624574b690be8bf3fc7ff9af3ab1
Alex Merced - AlexMercedCoder.com <alexmerced@alexmerced.com>
2019-12-13 22:31:01 -0500

    configuring postgres

--- api/models/comments.php
+++ api/models/comments.php
@@ -1,15 +1,14 @@
 <?PHP
 
-require_once '../../vendor/autoload.php';
+// require_once '../../vendor/autoload.php';
 
 $DBURL = getenv('DATABASE_URL');
 
-$dsn="psql:host=$DBURL;dbname=d3qvmqtmq9s8b user=cticedgggntdqf;user=cticedgggntdqf;port=5432;password=5d17168b471db3b178e8ede79d5f92605d765375ea153444cd403c0a544f2146";
+// $dsn="psql:host=$DBURL;dbname=d3qvmqtmq9s8b user=cticedgggntdqf;user=cticedgggntdqf;port=5432;password=5d17168b471db3b178e8ede79d5f92605d765375ea153444cd403c0a544f2146";
 
-$db = new PDO($dsn);
+// $db = new PDO($dsn);
 
-
-// $dbconn = pg_connect("host=$DBURL dbname=d3qvmqtmq9s8b user=cticedgggntdqf port=5432 password=5d17168b471db3b178e8ede79d5f92605d765375ea153444cd403c0a544f2146");
+$dbconn = pg_connect("host=$DBURL dbname=d3qvmqtmq9s8b user=cticedgggntdqf port=5432 password=5d17168b471db3b178e8ede79d5f92605d765375ea153444cd403c0a544f2146");
 // (id SERIAL, restid int, author varchar(30), comment varchar(140), password varchar(8), date timestamp DEFAULT now())
 
 class Comment {
