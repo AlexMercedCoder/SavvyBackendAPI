bf3c329767fd2f43491debb8d43931e8e07c6e97
Alex Merced - AlexMercedCoder.com <alexmerced@alexmerced.com>
2019-12-14 09:26:35 -0500

    configuring postgres

--- api/models/comments.php
+++ api/models/comments.php
@@ -2,12 +2,16 @@
 
 // require_once '../../vendor/autoload.php';
 
-$DBURL = getenv('DATABASE_URL');
+$h="ec2-174-129-253-27.compute-1.amazonaws.com";
+$n="d3qvmqtmq9s8b";
+$u="cticedgggntdqf";
+$p="5d17168b471db3b178e8ede79d5f92605d765375ea153444cd403c0a544f2146";
+$DBURL = "host=$h dbname=$n user=$u port=5432 password=$p";
 
 // $dsn="psql:host=$DBURL;dbname=d3qvmqtmq9s8b user=cticedgggntdqf;user=cticedgggntdqf;port=5432;password=5d17168b471db3b178e8ede79d5f92605d765375ea153444cd403c0a544f2146";
 
 // $db = new PDO($dsn);
-$dbconn = pg_connect(getenv('DATABASE_URL'));
+$dbconn = pg_connect($DBURL);
 // (id SERIAL, restid int, author varchar(30), comment varchar(140), password varchar(8), date timestamp DEFAULT now())
 
 class Comment {
