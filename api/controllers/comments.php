3be559762390badf60b0789f0bba3508f63b9ff0
Alex Merced - AlexMercedCoder.com <alexmerced@alexmerced.com>
2019-12-15 00:55:07 -0500

    cors fix

--- api/controllers/comments.php
+++ api/controllers/comments.php
@@ -1,8 +1,8 @@
 <?php
 
-
-header('Content-Type: application/json');
 header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+header("Access-Control-Allow-Headers: Content-Type");
 
 include_once '../models/comments.php';
 
